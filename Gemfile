df5f05c2946c10a8e42b65cef43fb0363571432a
Kris Sparks <kbs5280@gmail.com>
2016-10-31 17:24:16 -0600

    Gemfile update

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'bcrypt', '~> 3.1.7' # Use ActiveModel has_secure_password
 # gem 'capistrano-rails', group: :development
 gem "responders"
 gem "validate_url"
+gem "rake"
 
 group :development, :test do
   gem 'byebug', platform: :mri
--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,6 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
-
 module RailsNewSetup
   class Application < Rails::Application
     # Use the responders controller from the responders gem
