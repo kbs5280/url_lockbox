5362f20fce322cf2145324288f4aa579dc4d5d85
Kris Sparks <kbs5280@gmail.com>
2016-11-08 14:28:16 -0700

    Heroku has migration issues

--- db/migrate/20161108195502_add_column_to_urls.rb
+++
@@ -1,5 +0,0 @@
-# class AddColumnToUrls < ActiveRecord::Migration[5.0]
-#   def change
-#     remove_column :urls, :tag
-#   end
-# end
--- db/migrate/20161108212354_add_column_to_url.rb
+++ db/migrate/20161108212354_add_column_to_url.rb
@@ -1,4 +1,4 @@
-class AddTagColumnToUrls < ActiveRecord::Migration[5.0]
+class AddColumnToUrl < ActiveRecord::Migration[5.0]
   def change
     add_column :urls, :tag, :string, default: ""
   end
